af3181f7422a53b31b1426f1b35edeb24752ab9e
Maurice van Cooten <maurice@minus.nz>
2016-11-09 10:37:40 +1300

    forEach loop instead of for loop, decalre variables at the top

--- tabbed.js
+++ tabbed.js
@@ -1,44 +1,48 @@
 (function(root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define(['Tabbed'], factory);
-    } else if (typeof module === 'object' && module.exports) {
-        // Node. Does not work with strict CommonJS, but
-        // only CommonJS-like environments that support module.exports,
-        // like Node.
-        module.exports = factory();
-    } else {
-        // Browser globals (root is window)
-        root.Tabbed = factory();
-    }
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(['Tabbed'], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = factory();
+  } else {
+    // Browser globals (root is window)
+    root.Tabbed = factory();
+  }
 }(this, function() {
 
-    function Tabbed(el) {
-        var tabContent = document.querySelector(el).querySelectorAll('div'),
-            tabButton = document.querySelector(el).childNodes[1].querySelectorAll('a')
+  function Tabbed(el) {
+    var tabContent, tabButton, tabId
+    tabContent = document.querySelector(el).querySelectorAll('div')
+    tabButton = document.querySelector(el).childNodes[1].querySelectorAll('a')
 
-        for (var i = 1; i < tabContent.length; i++) {
-            tabContent[i].classList.add('hide')
-        }
-        for (var i = 0; i < tabButton.length; i++) {
-            tabButton[0].classList.add('active')
-            tabButton[i].addEventListener('click', function(e) {
-                e.preventDefault();
-                for (var i = 0; i < tabButton.length; i++) {
-                    tabButton[i].classList.remove('active')
-                }
-                this.classList.add('active')
-                for (var i = 0; i < tabContent.length; i++) {
-                    tabContent[i].classList.add('hide')
-                }
-                var tabId = this.getAttribute('href')
-                document.querySelector(tabId).classList.remove('hide')
-            })
-        }
-
-    }
-    // Just return a value to define the module export.
-    // This example returns an object, but the module
-    // can return a function as the exported value.
-    return Tabbed
+    tabContent.forEach(function(el, i) {
+      if (i > 0) {
+        el.classList.add('hide')
+      }
+    })
+    tabButton.forEach(function(el, i) {
+      if (i == 0) {
+        el.classList.add('active')
+      }
+      el.addEventListener('click', function(e) {
+        e.preventDefault();
+        tabButton.forEach(function(el) {
+          el.classList.remove('active')
+        })
+        this.classList.add('active')
+        tabContent.forEach(function(el) {
+          el.classList.add('hide')
+        })
+        tabId = this.getAttribute('href')
+        document.querySelector(tabId).classList.remove('hide')
+      })
+    })
+  }
+  // Just return a value to define the module export.
+  // This example returns an object, but the module
+  // can return a function as the exported value.
+  return Tabbed
 }));
